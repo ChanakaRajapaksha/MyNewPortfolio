f303cfdb3370159179e0cff87229fe1fa0e816cc
ChanakaRajapaksha <rcprajapaksha123@gmail.com>
2026-01-17 12:20:23 +0530

    implement resume window

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import { Draggable } from "gsap/Draggable";
 gsap.registerPlugin(Draggable);
 
 import { Navbar, Welcome, Dock } from "#components";
-import { Terminal, Safari } from "#windows/index.js";
+import { Terminal, Safari, Resume } from "#windows/index.js";
+
 import ClockWidget from "#components/ClockWidget.jsx";
 
 const App = () => {
   return (
-   <main>
-    <Navbar />
-    <ClockWidget />
-    <Welcome />
-    <Dock />
-    
-    <Terminal />
-    <Safari />
-   </main>
+    <main>
+      <Navbar />
+      <ClockWidget />
+      <Welcome />
+      <Dock />
+
+      <Terminal />
+      <Safari />
+      <Resume />
+    </main>
   );
 };
 
